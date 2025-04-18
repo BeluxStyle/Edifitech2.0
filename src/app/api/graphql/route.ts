305cdce22c802952bc499a13944267f3b79cba94
BeluxStyle <116590095+BeluxStyle@users.noreply.github.com>
2025-04-18 10:29:04 +0200

    cvb

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -21,6 +21,7 @@ export const POST = internalHandler;
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { getApolloServer } from '@/lib/apolloServer'; // Importar la función singleton
 import { getContextFromRequest } from '@/lib/graphql/getContextFromRequest';
+import { NextRequest } from 'next/server';
 
 // Obtener la instancia única de Apollo Server
 const apolloServer = getApolloServer();
@@ -33,5 +34,13 @@ const handler = startServerAndCreateNextHandler(apolloServer, {
   },
 });
 
+const createHandler =
+  (method: 'GET' | 'POST') =>
+  async (req: NextRequest, _context: any) => {
+    return handler(req);
+  };
+
+export const GET = createHandler('GET');
+export const POST = createHandler('POST');
+
 // Exportar el handler para GET y POST
-export { handler as GET, handler as POST };
